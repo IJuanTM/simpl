8bde9f362b3d3c9879c859530102f84f16edc5dd
IJuanTM <iwan2510@gmail.com>
2024-01-29 21:42:04 +0100

    Fix for errors

--- src/app/Config/app.php
+++ src/app/Config/app.php
@@ -3,12 +3,15 @@
 define('DEV', $_ENV['DEV']);
 
 // Timezone configuration
-const TIMEZONE = 'UTC';
+const TIMEZONE = 'UTC'; // https://www.php.net/manual/en/timezones.php
 
 // App configuration
 define('APP_NAME', $_ENV['APP_NAME']);
 define('APP_VERSION', $_ENV['APP_VERSION']);
 define('APP_URL', $_ENV['APP_URL']);
+
+// Last update
+define('LAST_UPDATE', $_ENV['LAST_UPDATE']);
 
 // Redirect configuration
 const REDIRECT = 'home';
