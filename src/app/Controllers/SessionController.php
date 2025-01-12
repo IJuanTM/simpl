7653106197395751b8c6e1089ad5f87ceca7b399
IJuanTM <iwan2510@gmail.com>
2025-01-12 14:29:26 +0100

    Added a SessionController for accessing SESSION variables

---
+++ src/app/Controllers/SessionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace app\Controllers;
+
+/**
+ * The SessionController class is the controller for accessing session variables.
+ * It contains methods for setting, getting, checking and removing session variables.
+ */
+class SessionController
+{
+    public function __construct()
+    {
+        // Start the session
+        session_start();
+    }
+
+    /**
+     * Method for setting a session variable.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return void
+     */
+    public static function set(string $key, mixed $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Method for getting a session variable.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public static function get(string $key): mixed
+    {
+        return self::has($key) ? $_SESSION[$key] : null;
+    }
+
+    /**
+     * Method for checking if a session variable exists.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public static function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    /**
+     * Method for removing a session variable.
+     *
+     * @param string $key
+     *
+     * @return void
+     */
+    public static function remove(string $key): void
+    {
+        unset($_SESSION[$key]);
+    }
+}
